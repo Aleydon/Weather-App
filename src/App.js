bca9d78ec72f42f54e136c8cc3eca4073350b076
Aleydon <aleydonroberto@gmail.com>
2020-07-19 14:35:52 -0300

    Added base code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 
 
 
 
 
 function App() {
-  return (
-    <div>
-      <h3>Nyaaa Nyaaa</h3>
-    </div>
-  );
+  //Verify if Location active
+  const [ location, setLocation  ] = useState(false);
+
+
+
+  //Get your GeoLocation
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation(true);
+    })
+  }, []);
+
+  
+  
+  if (location == false){
+    return(
+      <Fragment>
+          <h3>Voce Precisa habilitar a localizacao</h3>
+      </Fragment>
+    );
+  }else{
+   return (
+      <div>
+          <h3>Huehueheue</h3>
+      </div>
+    );
+  }
+
 }
 
 export default App;
